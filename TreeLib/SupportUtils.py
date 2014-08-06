bc181a48dc35cb35019eefbc40f03edbce90ce38
Jonathan Seguin <jo@jonathanseguin.com>
2014-08-06 12:48:24 -0400

    Small fix to keep backwards compatibility with python 2.6

--- TreeLib/SupportUtils.py
+++ TreeLib/SupportUtils.py
@@ -1,6 +1,6 @@
-""" 
+"""
 This script is a pipeline to use with the TreeClass tools.
-........................... 
+...........................
 Missing doc!!!!!!!!!!
 """
 import subprocess
@@ -17,12 +17,12 @@ phymlbootstrapmodel='_phyml_boot_stats.txt'
 phymltrees='_phyml_trees.txt'
 
 def executePipe(tree, nxsfile=None, fasta=None, al=0, type=None, treefile=None):
-	
+
 	n=[]
 	for leaf in tree:
 		if(len(n)<7):
 			n.append(leaf.name)
-	
+
 	tree.prune(n)
 
 	if(treefile is not None):
@@ -40,11 +40,11 @@ def executePipe(tree, nxsfile=None, fasta=None, al=0, type=None, treefile=None):
 
 	if(nxsfile is None):
 		if fasta is None:
-			print 
+			print
 			print "WRITING your sequence into a fasta file"
 			tree.writeSeqToFasta(comment=0)
 			fasta="seq.fasta"
-	
+
 		nxsfile=write_al_in_nxs_file(fasta, al=al)
 
 	executePhyML(nxsfile, treefile)
@@ -58,7 +58,7 @@ def write_al_in_nxs_file(fastafile, outnxs="seq.nxs", al=0):
 		subprocess.call(['clustalw', ('-INFILE='+fastafile), ('-OUTFILE='+outnxs), '-convert', '-OUTPUT=NEXUS'])
 
 	else:
-		print 
+		print
 		print 'Sequence not aligned!!'
 		print 'ALIGNING sequences with clustalw ...'
 		clustalcmd="clustalw -INFILE=" +fastafile+" -OUTFILE="+outnxs+ " -OUTPUT=NEXUS"
@@ -75,7 +75,7 @@ def write_al_in_nxs_file(fastafile, outnxs="seq.nxs", al=0):
 
 
 def executePhyML(seqfile, treefile, quiet=0):
-	print 
+	print
 	print "EXECUTING phyML ..."
 	phymlcmd= "phyml -i " + seqfile +" -u " + treefile + " -n 1 -o l --print_site_lnl --no_memory_check"
 	if(quiet):
@@ -111,43 +111,44 @@ def executeCMD(cmd):
 
 
 def nexrepair(nxsfile):
-	"""Repair nexus file for phyML"""
-	#print "REFORMATING your nexus file to phyML input ..."
-	with open(nxsfile, 'r') as infile, open("tmp", 'w') as outfile:
-		found_matrix = False
-		first_block_passed = False
-		newFileContent = ""
-		for line in infile:
-			line = line.replace("\n", "").strip()
-		  	lineup = line.upper()
-			ignoreLine = False
-			correctedLine = line 
-			if 'format missing=?'==line:
-				ignoreLine = True
-				#CLustal missing line found
-			
-			elif lineup == "MATRIX":
-				found_matrix = True
-				#MATRIX LINE found
-		    
-			if found_matrix and not first_block_passed and line == "":
-				first_block_passed = True
-		    
-			if line != "" and first_block_passed:
-				parts = line.split()
-				correctedLine = parts[-1]
-		  
-			if line==";":
-				newFileContent += ";"
-			elif not ignoreLine:
-				if newFileContent != "":
-					newFileContent += "\n"
-				newFileContent += correctedLine
+    """Repair nexus file for phyML"""
+    #print "REFORMATING your nexus file to phyML input ..."
+    with open(nxsfile, 'r') as infile:
+        with open("tmp", 'w') as outfile:
+            found_matrix = False
+            first_block_passed = False
+            newFileContent = ""
+            for line in infile:
+                line = line.replace("\n", "").strip()
+                lineup = line.upper()
+                ignoreLine = False
+                correctedLine = line
+                if 'format missing=?'==line:
+                    ignoreLine = True
+                    #CLustal missing line found
 
-		outfile.write(newFileContent)
+                elif lineup == "MATRIX":
+                    found_matrix = True
+                    #MATRIX LINE found
 
-	subprocess.call(['mv', "tmp", nxsfile])
-	return nxsfile
+                if found_matrix and not first_block_passed and line == "":
+                    first_block_passed = True
+
+                if line != "" and first_block_passed:
+                    parts = line.split()
+                    correctedLine = parts[-1]
+
+                if line==";":
+                    newFileContent += ";"
+                elif not ignoreLine:
+                    if newFileContent != "":
+                        newFileContent += "\n"
+                        newFileContent += correctedLine
+
+                outfile.write(newFileContent)
+
+    subprocess.call(['mv', "tmp", nxsfile])
+    return nxsfile
 
 if __name__ == '__main__':
 	ensemblTree=TreeUtils.fetch_ensembl_genetree_by_id(treeID="ENSGT00390000003602", output="phyloxml", aligned=1, sequence="cdna")
